46843dece1a95e840b1034079927b8ef9c017654
Dmitry <bens1iller@gmail.com>
2020-11-22 19:59:29 +0300

    Updates

---
+++ precache-manifest.9f8fbce10863fc2589b0124baa68911a.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d9fd945398c04e24ce12bc0dfd4bce",
+    "url": "/candyshop/index.html"
+  },
+  {
+    "revision": "f8b7848f5d65ae37e626",
+    "url": "/candyshop/static/css/main.0aad9656.chunk.css"
+  },
+  {
+    "revision": "aaa6eace9450bedac993",
+    "url": "/candyshop/static/js/2.7852d79e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/candyshop/static/js/2.7852d79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b7848f5d65ae37e626",
+    "url": "/candyshop/static/js/main.e20ba0f5.chunk.js"
+  },
+  {
+    "revision": "1d7f63d9a08e2ce9f77b",
+    "url": "/candyshop/static/js/runtime-main.ed6c1f0b.js"
+  },
+  {
+    "revision": "22f16521a7d16c7f8342680cf30b46bd",
+    "url": "/candyshop/static/media/Circe-Bold.22f16521.ttf"
+  },
+  {
+    "revision": "4b8b401a99c49f409edd340a1721c8e5",
+    "url": "/candyshop/static/media/Circe-Bold.4b8b401a.woff"
+  },
+  {
+    "revision": "fa54724c5f76114b7dd0234182ca2064",
+    "url": "/candyshop/static/media/Circe-Regular.fa54724c.woff"
+  },
+  {
+    "revision": "6e8a08cd59bed63dd5fd320bea904d38",
+    "url": "/candyshop/static/media/Circle-Regular.6e8a08cd.ttf"
+  },
+  {
+    "revision": "95b717bae19a123a9d1e8e8428d9fb81",
+    "url": "/candyshop/static/media/Intro_Script_R_H2_Base.95b717ba.ttf"
+  },
+  {
+    "revision": "2c10881bb5ec2ef2b0af0b65f27cf44e",
+    "url": "/candyshop/static/media/PlayfairDisplay-BoldItalic.2c10881b.woff"
+  },
+  {
+    "revision": "888818df7b25952ef1e68f9f12db9583",
+    "url": "/candyshop/static/media/PlayfairDisplay-BoldItalic.888818df.ttf"
+  },
+  {
+    "revision": "0fec3154e3f7e551a4f719373cf3c0ba",
+    "url": "/candyshop/static/media/PlayfairDisplaySC-Bold.0fec3154.ttf"
+  },
+  {
+    "revision": "c70abe4969e16ca2d75fcc5162eaac1e",
+    "url": "/candyshop/static/media/PlayfairDisplaySC-Bold.c70abe49.woff"
+  },
+  {
+    "revision": "6994369a62ede00c1360838c1d0e6954",
+    "url": "/candyshop/static/media/box-with-cake--desktop.6994369a.jpg"
+  },
+  {
+    "revision": "0e4c947467037eb69b8163b37489b52c",
+    "url": "/candyshop/static/media/box-with-cake--mobile.0e4c9474.jpg"
+  },
+  {
+    "revision": "9326015691983da201dfef55d61d1873",
+    "url": "/candyshop/static/media/box-with-cake--tablet.93260156.jpg"
+  },
+  {
+    "revision": "8cc53b497af459880405849a25020cf5",
+    "url": "/candyshop/static/media/cake-child--desktop.8cc53b49.jpg"
+  },
+  {
+    "revision": "03cef93b46a34ab080c6289fad90b296",
+    "url": "/candyshop/static/media/cake-child--mobile.03cef93b.jpg"
+  },
+  {
+    "revision": "4a98fb35508890af56bcf04e7311ad4e",
+    "url": "/candyshop/static/media/cake-child--tablet.4a98fb35.jpg"
+  },
+  {
+    "revision": "0874d2a84ccb6402cd945309ff331911",
+    "url": "/candyshop/static/media/cake-classic--desktop.0874d2a8.jpg"
+  },
+  {
+    "revision": "f3a7844519e3d2e1906253eeda50f13f",
+    "url": "/candyshop/static/media/cake-classic--mobile.f3a78445.jpg"
+  },
+  {
+    "revision": "ebecd5989e410ca2207d292eaaace5ff",
+    "url": "/candyshop/static/media/cake-classic--tablet.ebecd598.jpg"
+  },
+  {
+    "revision": "908c046578b7c343c4e2aa4ad74b8b5c",
+    "url": "/candyshop/static/media/cake-male--desktop.908c0465.jpg"
+  },
+  {
+    "revision": "fc11d9bdac8a648215656379e2e090ff",
+    "url": "/candyshop/static/media/cake-male--mobile.fc11d9bd.jpg"
+  },
+  {
+    "revision": "3566ab65bf6fab23ef1ef571506ae125",
+    "url": "/candyshop/static/media/cake-male--tablet.3566ab65.jpg"
+  },
+  {
+    "revision": "418c09e4b849fa926c6786314bd26d33",
+    "url": "/candyshop/static/media/cake-mousse--desktop.418c09e4.jpg"
+  },
+  {
+    "revision": "4dc3a94d1fcdf926af3251e7b083a11e",
+    "url": "/candyshop/static/media/cake-mousse--mobile.4dc3a94d.jpg"
+  },
+  {
+    "revision": "75e43ff3cf9e31e36e771ed8bd254733",
+    "url": "/candyshop/static/media/cake-mousse--tablet.75e43ff3.jpg"
+  },
+  {
+    "revision": "b40f6850022584d6908117421ba95ce1",
+    "url": "/candyshop/static/media/cake-wedding--desktop.b40f6850.jpg"
+  },
+  {
+    "revision": "a63ed7ab6d7ab1ca2d18b26e0be9acd8",
+    "url": "/candyshop/static/media/cake-wedding--mobile.a63ed7ab.jpg"
+  },
+  {
+    "revision": "8c54abe5eafd6e4d987d66d4cb657d38",
+    "url": "/candyshop/static/media/cake-wedding--tablet.8c54abe5.jpg"
+  },
+  {
+    "revision": "31600166211f190c946f5e5404cb04ab",
+    "url": "/candyshop/static/media/cake-woman--desktop.31600166.jpg"
+  },
+  {
+    "revision": "717b6bf9fd7f8ea6ef27d513082ed7c4",
+    "url": "/candyshop/static/media/cake-woman--mobile.717b6bf9.jpg"
+  },
+  {
+    "revision": "0fdb043674bc3f0c736f10ae08b2a8d4",
+    "url": "/candyshop/static/media/cake-woman--tablet.0fdb0436.jpg"
+  }
+]);
